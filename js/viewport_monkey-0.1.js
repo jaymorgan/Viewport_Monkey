563262f94e07c282d8d469842aacfc5bcfa02398
Jason Morgan <jay@jmorgan.ws>
2012-07-05 11:57:07 -0400

    Broke Out Code to the JS file
    Removed the javascript from the HTML file and added it to the JS file.

--- js/viewport_monkey-0.1.js
+++ js/viewport_monkey-0.1.js
@@ -1,23 +1,31 @@
-
-
-	function viewportMonkeyInit() {
-		var numsDiv = "<div class='nums'>WINDOW SIZE: <input type='counter' name='wSRcounter' class='monkeyReadout' value='-init-' /></div>";
-		$(numsDiv).appendTo($('body'));
-		
-		var win = $(window);
-		var windwidth;
-		
-		win.on('resize', function() {
-			calculateSize();
-		});
-		
-		function resizeWindow() {
-			$('input.wSRcounter').val(windwidth);
-		};
-		function calculateSize() {
-			windwidth = win.width();
-			resizeWindow();
-		}
+function viewportMonkeyInit() {
+	var numsDiv = "<div class='monkeynums'>Viewport Size: <input type='counter' name='wSRcounter' class='monkeyReadout' value='-init-' /></div>";
+	$(numsDiv).appendTo($('body'));
+	
+	$('.monkeynums').css({
+		'background-color' : 'black',
+		'font-family': 'sans-serif',
+		'font-weight' : 'bolder',
+		'position': 'fixed',
+		'bottom': '0px',
+		'left': '0px',
+		'color': 'white',
+		'padding': '8px'
+	});
+	
+	var win = $(window);
+	var windwidth;
+	
+	win.on('resize', function() {
+		calculateSize();
+	});
+	
+	function resizeWindow() {
+		$('input.monkeyReadout').val(windwidth);
+	};
+	function calculateSize() {
+		windwidth = win.width();
+		resizeWindow();
 	}
-	viewportMonkeyInit();
-
+}
+viewportMonkeyInit();
