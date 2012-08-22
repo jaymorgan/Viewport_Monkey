8935d5bba0dc99c9d035785123d08ada73429f82
Jason Morgan <jay@jmorgan.ws>
2012-08-22 14:04:18 -0400

    Added a Stylesheet and Positioning Functionality

--- js/viewport_monkey-0.1.js
+++ js/viewport_monkey-0.1.js
@@ -1,17 +1,15 @@
-function viewportMonkeyInit() {
-	var numsDiv = "<div class='monkeynums'>	Viewport Width:<input type='counter' name='wSRcounter' class='monkeyReadout' value='-init-' /><select id='monkeyUnits'><option value='px'>px</option><option value='em'>em</option></select><button class='changepos'>move</button></div>";
+function vpm_viewportMonkeyInit() {
+	var vpm_numsDiv = "<div class='positiontopleft vpm_monkeynums'>	Viewport Width:<input type='counter' name='wSRcounter' class='vpm_monkeyReadout' value='-init-' /><select id='vpm_monkeyUnits'><option value='px'>px</option><option value='em'>em</option></select><button class='vpm_changepos'>move</button></div>";
 	
-	$(numsDiv).appendTo($('body'));
+	$(vpm_numsDiv).appendTo($('body'));
 	
-	var monkeynums = $('.monkeynums');
+	var vpm_monkeynums = $('.vpm_monkeynums');
 	
-	monkeynums.css({
+	vpm_monkeynums.css({
 		'background-color' : 'black',
 		'font-family': 'sans-serif',
 		'font-weight' : 'bolder',
-		'position': 'fixed',
-		'bottom': '-6px',
-		'left': '-6px',
+		'position': 'absolute',
 		'color': 'white',
 		'padding': '14px',
 		'-moz-border-radius': '6px',
@@ -21,51 +19,66 @@ function viewportMonkeyInit() {
 		'border-radius': '6px'
 	});
 	
-	var monkeySelector = $('#monkeyUnits');
-	var win = $(window);
-	var windwidth;
-	var movebtn = $('.changepos');
+	var vpm_monkeySelector = $('#vpm_monkeyUnits');
+	var vpm_win = $(window);
+	var vpm_windwidth;
+	var vpm_movebtn = $('.vpm_changepos');
 	
-	win.on('resize', function() {
-		calculateSize();
+	vpm_win.on('resize', function() {
+		vpm_calculateSize();
 	});
 	
-	win.on('load', function() {
-		calculateSize();
+	vpm_win.on('load', function() {
+		vpm_calculateSize();
 	});
 	
-	monkeySelector.on('change', function(){
-		calculateSize();
+	vpm_monkeySelector.on('change', function(){
+		vpm_calculateSize();
 	});
 	
-	movebtn.on('click', function(event){
-		monkeynums.css({
-			'background-color' : 'grey',
-			'bottom': '',
-			'top': '-6px',
-			'left': '-6px'
-		});
-		
-	});
-	
-	function windowWidthOutput() {
-		$('input.monkeyReadout').val(windwidth);
+	function vpm_windowWidthOutput() {
+		$('input.vpm_monkeyReadout').val(vpm_windwidth);
 	};
 	
-	function calculateSize() {
-		windwidth = win.width();
-		var units = checkMonkeyUnits();
-		if (units == 'em') {
-			var emSize = $('body').css('font-size');
-			var emWidth = windwidth / parseInt(emSize);
-			windwidth = emWidth; 
+	// Pulls the size of the viewport and converts to EMs if its set to show EMs
+	function vpm_calculateSize() {
+		vpm_windwidth = vpm_win.width();
+		var vpm_units = vpm_checkMonkeyUnits();
+		// If the dropdown is set to EMs this if capturesthat and translated the numbers to EMs
+		if (vpm_units == 'em') {
+			var vpm_emSize = $('body').css('font-size');
+			var vpm_emWidth = vpm_windwidth / parseInt(vpm_emSize);
+			vpm_windwidth = vpm_emWidth; 
 		}
-		windowWidthOutput();
+		vpm_windowWidthOutput();
 	}
 	
-	function checkMonkeyUnits() {
-		var currentUnits = monkeySelector.val();
-		return currentUnits;
+	// Checks to see what the units dropdown is set to
+	function vpm_checkMonkeyUnits() {
+		var vpm_currentUnits = vpm_monkeySelector.val();
+		return vpm_currentUnits;
 	}
+	
+	// Array of CSS Class names to move the Monkey around the screen
+	var positions = ['positiontopleft', 'positionbottomleft', 'positionbottomright', 'positiontopright'];
+	//	Stores the current index of the positions array
+	var currentPosition = 0;
+	
+	function checkPostion(){
+	  var oldClass = positions[currentPosition];
+	  currentPosition++;
+	  if(currentPosition >= positions.length){
+	  	currentPosition =0;
+	  } 
+	    var newClass = positions[currentPosition];
+	    vpm_monkeynums.removeClass(oldClass);
+	    vpm_monkeynums.addClass(newClass);
+	}
+	
+	vpm_movebtn.on("click", function(event){
+		checkPostion();
+	  vpm_monkeynums.css('background-color', '#ff5a00');
+	});
+	
 }
-viewportMonkeyInit();
+
