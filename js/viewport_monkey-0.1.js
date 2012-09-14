f87379b4dd6f7d9eb4896c6d04bcd37d2dafa2da
Jason Morgan <jay@jmorgan.ws>
2012-09-14 15:59:20 -0400

    Added some Monkey art other tweaks

--- js/viewport_monkey-0.1.js
+++ js/viewport_monkey-0.1.js
@@ -1,28 +1,70 @@
-function vpm_viewportMonkeyInit() {
-	
+/*
+	Viewport Monkey v0.2
+	Author: Jason Morgan
+	Twitter: @jaymorgan
+*/
+
+// Send properties as "bottom", "top" for the vert prop
+// Send "left" and "right" for the horizontal prop
+// If not set or not set coeerctly they will default to the bottom left
+
+
+function vpm_viewportMonkeyInit(initial_position_vert, initial_position_hori) {
+		
 	$(document).ready(function () {
 	    $('head').append('<link rel="stylesheet" href="css/vpm_ViewportMonkeyStyles.css" type="text/css" />');
 	});
 	
-	var vpm_numsDiv = "<div class='positiontopleft vpm_monkeynums'>	Viewport Width:<input type='counter' name='wSRcounter' class='vpm_monkeyReadout' value='-init-' /><select id='vpm_monkeyUnits'><option value='px'>px</option><option value='em'>em</option></select><button class='vpm_changepos'>move</button></div>";
+	var vpm_numsDiv = "<div class='vpm_monkeynums'>	Viewport Width:<input type='counter' name='wSRcounter' class='vpm_monkeyReadout' value='-init-' /><select id='vpm_monkeyUnits'><option value='px'>px</option><option value='em'>em</option></select><button class='vpm_changepos'>move</button><img src=''/></div>";
 	
 	$(vpm_numsDiv).appendTo($('body'));
 	
 	var vpm_monkeynums = $('.vpm_monkeynums');
 	
 	vpm_monkeynums.css({
-		'background-color' : 'black',
+		'background-color' : 'rgba(0,0,0,0.4)',
 		'font-family': 'sans-serif',
 		'font-weight' : 'bolder',
 		'position': 'absolute',
 		'color': 'white',
-		'padding': '14px',
+		'padding': '8px',
 		'-moz-border-radius': '6px',
 		'-webkit-border-radius': '6px',
 		'-ms-border-radius': '6px',
 		'-o-border-radius': '6px',
 		'border-radius': '6px'
 	});
+	
+	var initposvert = initial_position_vert;
+	var initposhori = initial_position_hori;
+	
+	if(initposvert === "bottom"){
+		if (initposhori === "left") {
+			initpos = "bottomleft";
+		} else if (initposhori === "right") {
+			initpos = "bottomright";
+		} else {
+			// Something went wrong!
+			// Set to default
+			initpos = "bottomleft";
+		}
+	} else if (initposvert === "top") {
+		if (initposhori === "left") {
+			initpos = "topleft";
+		} else if (initposhori === "right") {
+			initpos = "topright";
+		} else {
+			// Something went wrong!
+			// Set to default
+			initpos = "bottomleft";
+		}
+	} else {
+		// Something went wrong!
+		// Set to default
+		initpos = "bottomleft";
+	}
+	
+	console.log('initpos = ' + initpos);
 	
 	var vpm_monkeySelector = $('#vpm_monkeyUnits');
 	var vpm_win = $(window);
@@ -64,8 +106,28 @@ function vpm_viewportMonkeyInit() {
 		return vpm_currentUnits;
 	}
 	
+	// Found this at CSS-Tricks.com
+	// http://css-tricks.com/snippets/javascript/javascript-array-contains/
+	Array.prototype.contains = function ( needle ) {
+	   for (i in this) {
+	       if (this[i] == needle) return i;
+	   }
+	   return 'x';
+	}
+	
 	// Array of CSS Class names to move the Monkey around the screen
 	var positions = ['positiontopleft', 'positionbottomleft', 'positionbottomright', 'positiontopright'];
+	
+	// Find the position as sent from the props at top
+	
+	var postest = positions.contains('position'+initpos);
+	
+	if (postest != 'x') {
+	   vpm_setNewClass(positions[postest]);
+	} else {
+		vpm_setNewClass(positions[0]);
+	}
+	
 	//	Stores the current index of the positions array
 	var currentPosition = 0;
 	
@@ -75,15 +137,24 @@ function vpm_viewportMonkeyInit() {
 	  if(currentPosition >= positions.length){
 	  	currentPosition =0;
 	  } 
-	    var newClass = positions[currentPosition];
-	    vpm_monkeynums.removeClass(oldClass);
-	    vpm_monkeynums.addClass(newClass);
+	  var newClass = positions[nclass];
+	  vpm_monkeynums.removeClass(oldClass);
+	  vpm_setNewClass(newClass);  
+	}
+	
+	function vpm_setNewClass(newClass) {
+		
+		vpm_monkeynums.addClass(newClass);
 	}
 	
 	vpm_movebtn.on("click", function(event){
 		checkPostion();
 	  vpm_monkeynums.css('background-color', '#ff5a00');
 	});
+	
+	function vpm_findInArray(a, x){
+		
+	}
 	
 }
 
